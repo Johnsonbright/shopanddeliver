a3db833100d5874afe6ec756daa7bc75ebf32425
Ifechukwu Johnson Uzoma <johnsonbright101@gmail.com>
2025-02-11 20:05:34 +0100

    fixing issue with splashscreen configuration

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Platform } from 'react-native'
+import React, { useEffect } from 'react'
 import AppNavigation from './src/Navigation/AppNavigation';
+import SplashScreen from 'react-native-splash-screen';
 
 
 
 const App = () => {
+ useEffect(()  => {
+  if(Platform.OS === "android")
+  SplashScreen.hide();
+ },[])
+
   return (
    <AppNavigation/>
   )
